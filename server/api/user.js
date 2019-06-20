0dd330b7a97b4bf9d147ebcb21fc001c9f6e7f55
Marcia <mwan0906@gmail.com>
2019-06-19 23:21:39 -0400

    api permissions

--- server/api/user.js
+++ server/api/user.js
@@ -1,15 +1,18 @@
 const router = require('express').Router();
 const { User, Stock } = require('../db/models');
 
-const axios = require('axios');
+const userGateway = (req, res, next) => {
+  if (req.params.userId != req.session.passport.user) res.send('Forbidden');
+  else next();
+}
 
-router.get('/:userId', (req, res, next) => {
+router.get('/:userId', userGateway, (req, res, next) => {
   User.findByPk(req.params.userId)
     .then(user => user.getStocks({attributes: ['name']}))
     .then(stocks => res.send(stocks));
 });
 
-router.post('/:userId', async (req, res, next) => {
+router.post('/:userId', userGateway, async (req, res, next) => {
   Promise.all([
     User.findByPk(req.params.userId),
     Stock.findOrCreate({

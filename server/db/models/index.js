ee18925fc9cc448f38205652717d8439a297bb8c
Marcia <mwan0906@gmail.com>
2019-06-19 22:23:30 -0400

    refactored

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -5,20 +5,24 @@ const Line = require('./line');
 User.belongsToMany(Stock, { through: 'line' });
 
 User.prototype.buyStock = async function(stock, price, amountBought = 1) {
-   
-  const line = await Line.findOrCreate({
-    where: {
-      userId: this.id,
-      stockName: stock.name
-    },
-    defaults: {
-      quantity: amountBought
-    }
-  });
-  if (!line[1]) await line[0].update({
-      quantity: line[0].quantity + amountBought
-  });
-  await this.update({ balance: this.balance - (price * amountBought) });
+  await Promise.all([
+    Line.findOrCreate({
+      where: {
+        userId: this.id,
+        stockName: stock.name
+      },
+      defaults: {
+        quantity: amountBought
+      }
+    }).then(line => {
+      if (!line[1])
+        return line[0].update({
+          quantity: line[0].quantity + amountBought
+        });
+    }),
+    this.update({ balance: this.balance - price * amountBought })
+  ]);
+
   return this.getStocks({ attributes: ['name'] });
 };
 
